c8d341bacf7062f32f0eda6deb8367aba4867375
nhatphuong <nlnhatphuong@gmail.com>
2026-03-24 21:01:51 +0700

    Merge Day004 branch

---
+++ Day 4/Lists/task.py
@@ -0,0 +1,29 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey",
+                     "Georgia", "Connecticut", "Massachusetts",
+                     "Maryland", "South Carolina", "New Hampshire",
+                     "Virginia", "New York", "North Carolina",
+                     "Rhode Island", "Vermont", "Kentucky",
+                     "Tennessee", "Ohio", "Louisiana", "Indiana",
+                     "Mississippi", "Illinois", "Alabama", "Maine",
+                     "Missouri", "Arkansas", "Michigan", "Florida",
+                     "Texas", "Iowa", "Wisconsin", "California",
+                     "Minnesota", "Oregon", "Kansas", "West Virginia",
+                     "Nevada", "Nebraska", "Colorado", "North Dakota",
+                     "South Dakota", "Montana", "Washington", "Idaho",
+                     "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona",
+                     "Alaska", "Hawaii"]
+print(states_of_america[0])
+print(states_of_america[2])
+print(states_of_america[-1])
+'''
+fruits = ["Cherry", "Apple", "Pear"]
+print(fruits)
+fruits[0] = "Orange"
+print(fruits)
+# fruits will now become ["Orange", "Apple", "Pear"]
+'''
+fruits = ["Cherry", "Apple", "Pear"]
+print(fruits)
+fruits.append("Orange")
+print(fruits)
+# fruits will now become ["Cherry", "Apple", "Pear", "Orange"]
