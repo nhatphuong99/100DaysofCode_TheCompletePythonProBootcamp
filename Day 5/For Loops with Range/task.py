e7163dbd6b9cea567f875502b9e391caf339aa48
nhatphuong <nlnhatphuong@gmail.com>
2026-03-24 20:57:03 +0700

    Complerw Day 5 - Python Loops

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,10 @@
+for number in range(1, 10):
+    print(number)
+# print out each of the numbers 1 - 9
+# a <= range(a, b) < b
+
+gauss = 0
+for i in range(1, 100):
+    gauss += i
+
+print(gauss)
--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,24 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 print(range(1, 10))
+'''
+student_scores = [180, 124, 165]
+
+sum = 0
+for score in student_scores:
+    sum += score
+    
+print(sum)
+'''
+
+scores = [8, 65, 89, 86, 55, 91, 64, 89]
+max = scores[0]
+for score in scores:
+    if score > max:
+        max  = score
+print(max)
+
+min = scores[0]
+for score in scores:
+    if score < min:
+        min = score
+print(min)
