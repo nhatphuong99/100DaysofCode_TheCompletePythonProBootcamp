4383f8016a4aa276c431e008ac6cc101eda27471
nhatphuong <nlnhatphuong@gmail.com>
2026-03-31 20:44:37 +0700

    Complete Day13 - Debugging

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -8,5 +8,14 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+"""
+loop through the number 1 to 20
+"""
 # 2. When is the function meant to print "You got it"?
+"""
+when the loop reach 20
+"""
 # 3. What are your assumptions about the value of i?
+"""
+the i is actually in range [1,19] (range i [a, b))
+"""
--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,7 @@
 age = int(input("How old are you?"))
+
+
+
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at age {age}.")
+
