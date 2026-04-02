ad8cd52d86794b080a1e93d72be294391eb698e1
nhatphuong <nlnhatphuong@gmail.com>
2026-04-02 21:10:54 +0700

    Complete Day 15 - Coffee Machine

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -30,3 +30,14 @@ resources = {
     "coffee": 100,
 }
 
+
+"""
+# Coffee Machine Program Requirements
+- Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+- Turn off the Coffee Machine by entering “off” to the prompt. 
+- Print report. 
+- Check resources sufficient? 
+- Process coins. 
+- Check transaction successful?
+- Make Coffee.
+"""
